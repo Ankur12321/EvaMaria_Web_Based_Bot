fb3dba30fc77c02f5690fdcaec8e9359935fa46f
Ankur12321 <84258110+Ankur12321@users.noreply.github.com>
2023-03-17 08:25:32 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/Bamel_Backup')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/Bamel_Bots')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
